28375953b9c3ef457d0a65137b5a4327a0e4f155
challavishnuvardhanreddy <challavishnuvardhancvvr@gmail.com>
2025-01-27 10:43:06 +0530

    Message: After Authentication and JwtException handling

--- securityOnePosts/src/main/java/com/example/securityOnePosts/Filter/JwtAuthFilter.java
+++ securityOnePosts/src/main/java/com/example/securityOnePosts/Filter/JwtAuthFilter.java
@@ -8,11 +8,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
@@ -22,30 +25,38 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserService userService;
+
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver handlerExceptionResolver;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        final String requestToken=request.getHeader("Authorization");
-        if(requestToken==null || !requestToken.startsWith("Bearer"))
-        {
-            filterChain.doFilter(request,response);
-            return;
+        try {
+            final String requestToken = request.getHeader("Authorization");
+            if (requestToken == null || !requestToken.startsWith("Bearer")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            //  String token=requestToken.split("Bearer")[1];
+            // Extract and trim the token
+            String token = requestToken.substring(7).trim(); // Remove "Bearer " and trim any whitespace
+            Long userId = jwtService.getUserIdFromToken(token);
+            //SecurityContextHolder.getContext().getAuthentication()==null we are adding this because, once we set the
+            // context still there are chances that we come back and hit this, so no need to check again
+            if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                User user = userService.getUserDetails(userId);
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user, null, null);
+                //adding few more details to authentication token, not necessary
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+            //passing the request to the next filter
+            filterChain.doFilter(request, response);
         }
-      //  String token=requestToken.split("Bearer")[1];
-        // Extract and trim the token
-        String token = requestToken.substring(7).trim(); // Remove "Bearer " and trim any whitespace
-        Long userId=jwtService.getUserIdFromToken(token);
-        //SecurityContextHolder.getContext().getAuthentication()==null we are adding this because, once we set the
-        // context still there are chances that we come back and hit this, so no need to check again
-        if(userId!=null && SecurityContextHolder.getContext().getAuthentication()==null)
+        catch (Exception ex)
         {
-            User user=userService.getUserDetails(userId);
-            UsernamePasswordAuthenticationToken authenticationToken=new UsernamePasswordAuthenticationToken(user,null,null);
-            //adding few more details to authentication token, not necessary
-            authenticationToken.setDetails( new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            handlerExceptionResolver.resolveException(request,response,null,ex);
         }
-        //passing the request to the next filter
-        filterChain.doFilter(request,response);
     }
 
 }
--- securityOnePosts/src/main/java/com/example/securityOnePosts/Services/UserService.java
+++ securityOnePosts/src/main/java/com/example/securityOnePosts/Services/UserService.java
@@ -4,6 +4,7 @@ import com.example.securityOnePosts.Entities.User;
 import com.example.securityOnePosts.Repositories.UserRepository;
 import com.example.securityOnePosts.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,7 +19,7 @@ public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username).orElseThrow(()-> new ResourceNotFoundException("user with email:"+username+"not found!"));
+        return userRepository.findByEmail(username).orElseThrow(()-> new BadCredentialsException("user with email:"+username+"not found!"));
     }
 
     public User getUserDetails(Long id)
